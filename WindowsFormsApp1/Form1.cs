c5c096f261b76732b4fb221e7b7f51db4c90899a
Whatislav4ik <123952222+Whatislav4ik@users.noreply.github.com>
2023-02-02 15:20:33 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            counts = 0;
+        }
+
+        public static String censor(String text,
+                     String word)
+        {
+
+            // Разделение по пробелу
+            String[] word_list = text.Split(' ');
+
+            String result = "";
+
+            // цензурирование слова * по его длине
+            String stars = "";
+            for (int i = 0; i < word.Length; i++)
+                stars += '*';
+
+            // соединение 
+            int index = 0;
+            foreach (String i in word_list)
+            {
+                if (i.CompareTo(word) == 0)
+
+                    word_list[index] = stars;
+                index++;
+            }
+
+            // соединение слов
+            foreach (String i in word_list)
+                result += i + " ";
+
+            return result;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Equals("") || richTextBox1.Text.Equals(""))
+                label4.Text = "Повторите ввод!";
+            string ssd = string.Copy(textBox1.Text);
+            string asd = string.Copy(richTextBox1.Text);
+            string sad = censor(asd, ssd);
+            richTextBox2.Clear();
+            richTextBox2.AppendText(sad);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Clear();
+                StreamReader sr = File.OpenText(openFileDialog1.FileName);
+
+                string line = null;
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    richTextBox1.AppendText(line);
+                    richTextBox1.AppendText("\r\n");
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            StreamWriter sw = File.CreateText(saveFileDialog1.FileName + ".txt");
+
+            // Чтение строк с richTextBox1 и добавление их в файл
+            string line;
+            for (int i = 0; i < richTextBox2.Lines.Length; i++)
+            {
+                //  Чтение одной строки
+                line = richTextBox2.Lines[i].ToString();
+
+                //  Добавление этой строки в файл
+                sw.WriteLine(line);
+
+            }
+            //  Закрыть объект sw
+            sw.Close();
+        }
+
+        int counts;
+ 
+        public void Button4_Click(object sender, EventArgs e)
+        {
+
+            Form2 newfrm = new Form2();
+            newfrm.Show();
+            counts++;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static partial class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+
+            public static void Main(String[] args)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+               /* String extract = "GeeksforGeeks is a computer science " +
+                                 "portal for geeks. I am pursuing my " +
+                                 "major in computer science. ";
+#pragma warning disable CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+                String cen = ;
+#pragma warning restore CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+#pragma warning disable CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.
+                Console.WriteLine(censor(extract, cen));
+#pragma warning restore CS8604 // Возможно, аргумент-ссылка, допускающий значение NULL.*/
+            }
+        }
+    }
